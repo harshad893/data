0527c0ba6dd9bbd952e50627b44f972a42476b1e
reddyyuvakishore1211 <79675559+reddyyuvakishore1211@users.noreply.github.com>
2021-09-21 13:07:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from app.forms import *
+
+def student(request):
+    form=StudentForm()
+    if request.method=='POST':
+        form_data=StudentForm(request.POST)
+        if form_data.is_valid():
+            return HttpResponse('Form is Validated Succeefully')
+
+    return render(request,'student.html',context={'form':form})
